1bb11e362a3286351922985c1fde88706ae71d49
liuyangming <liuyangming@gmail.com>
2018-04-24 15:31:36 +0800

    修复CORS请求时出现ClassCastException异常的问题

--- bytejta-supports-springcloud/src/main/java/org/bytesoft/bytejta/supports/springcloud/web/TransactionHandlerInterceptor.java
+++ bytejta-supports-springcloud/src/main/java/org/bytesoft/bytejta/supports/springcloud/web/TransactionHandlerInterceptor.java
@@ -53,7 +53,7 @@ public class TransactionHandlerInterceptor implements HandlerInterceptor, Transa
 	@SuppressWarnings("deprecation")
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		if (HandlerMethod.class.isInstance(handler) == false) {
-			logger.warn("CompensableHandlerInterceptor cannot handle current request(uri= {}, handler= {}) correctly.",
+			logger.warn("TransactionHandlerInterceptor cannot handle current request(uri= {}, handler= {}) correctly.",
 					request.getRequestURI(), handler);
 			return true;
 		}
